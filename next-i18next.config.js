f4334a88e2c00d297e2acc3cba579797c12a01d7
maelnilba <mael.rioux1997@outlook.fr>
2022-06-02 20:24:29 +0200

    add vercel path config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "fr",
     locales: ["fr", "en"],
     localeDetection: true,
+    localePath: path.resolve("./public/locales"),
   },
 };
--- src/pages/ranked/index.tsx
+++ src/pages/ranked/index.tsx
@@ -5,6 +5,10 @@ import { Spectator } from "../../components/ranked/Spectator";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { useDevice } from "../../hooks/useDevice";
+import { NextApiRequestCookies } from "next/dist/server/api-utils";
+import { ParsedUrlQuery } from "querystring";
+import { IncomingMessage, ServerResponse } from "http";
+import { GetServerSidePropsResult, PreviewData } from "next";
 
 const Index = ({ ip }) => {
   const { t } = useTranslation(["ranked", "common"]);
@@ -30,14 +34,40 @@ const Index = ({ ip }) => {
   );
 };
 
-export async function getServerSideProps({ req, locale }) {
+type CustomGetServerSideProps<
+  P extends { [key: string]: any } = { [key: string]: any },
+  Q extends ParsedUrlQuery = ParsedUrlQuery
+> = (
+  context: GetServerSidePropsContext<Q>
+) => Promise<GetServerSidePropsResult<P>>;
+
+type GetServerSidePropsContext<Q extends ParsedUrlQuery = ParsedUrlQuery> = {
+  req: IncomingMessage & {
+    cookies: NextApiRequestCookies;
+  };
+  res: ServerResponse;
+  params?: Q;
+  query: ParsedUrlQuery;
+  preview?: boolean;
+  previewData?: PreviewData;
+  resolvedUrl: string;
+  locale: string; // This is where the magic happens.
+  locales?: string[];
+  defaultLocale?: string;
+};
+
+export const getServerSideProps: CustomGetServerSideProps = async ({
+  req,
+  locale,
+}) => {
+  const fs = require("fs");
   const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
   return {
     props: {
       ip,
       ...(await serverSideTranslations(locale, ["common", "ranked"])),
-    }, // will be passed to the page component as props
+    },
   };
-}
+};
 
 export default Index;
